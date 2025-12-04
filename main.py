c0fb91592eab183b4d28c8521e6c6dd19e97202a
Ferrett <cassidy.gause33@gmail.com>
2025-12-04 16:50:31 -0700

    main.py update to read frankenstein

--- main.py
+++ main.py
@@ -1,1 +1,6 @@
-print("greetings boots")
+def get_book_text(file_path):
+    with open(file_path) as f:
+        return f.read()
+def main():
+    text = get_book_text("books/frankenstein.txt")
+    print(text)
