cdbee427f643a8d400d913f235cdef8ffbbf98d3
Ferrett33 <cassidy.gause33@gmail.com>
2025-12-05 21:11:12 -0700

    Working Bookbot

--- main.py
+++ main.py
@@ -1,11 +1,15 @@
+from stats import number_of_words, number_of_characters
+
 def get_book_text(file_path):
-    with open(file_path) as f:
+    with open(file_path) as f: 
         return f.read()
-
-from stats import number_of_words
-
+    
 def main():
-    text = get_book_text("books/frankenstein.txt")
-    number_of_words(text)
+    text = get_book_text("books/frankenstein.txt") 
+    
+    words = number_of_words(text)
+    print(f"Found {words} total words.")
+    char_count = number_of_characters(text)
+    print(char_count)
 
 main()
--- stats.py
+++ stats.py
@@ -1,4 +1,13 @@
-def number_of_words(text):
-    words = text.split()
-    print(f"Found {len(words)} total words")
-    return len(words)
+def number_of_words(text): # Function to count number of words
+    words = text.split() # Split text into words
+    return len(words) # Return the count of words
+
+def number_of_characters(text):
+    ch_count = {} # Initialize empty dictionary
+    for ch in text:
+        ch = ch.lower() # Normalize to lowercase      
+        if ch in ch_count: 
+            ch_count[ch] += 1  # Increment count
+        else:
+            ch_count[ch] = 1 # Initialize count               
+    return ch_count # Return the character count dictionary
